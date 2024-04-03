4bb0f8a8bfad6a90efb06828772723dc5c06c453
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-04-03 14:31:48 +0530

    header section added with styling

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Heading = () => {
-  return <h1>Hunger Feast</h1>;
+/*
+*Applayout
+*   -Header
+        -logo
+        -navbar items
+*   -body
+        -seach bar
+        -reastaurant container
+            -res cards
+                -img
+                -name
+                -ratings
+                -food item
+
+    -footer
+        -copyright
+        -links
+        -address
+        -contactdetails
+*/
+
+const Header = () => {
+  return(
+    <div className="header">
+        <div className="logo">
+            <img src="./images/logo" alt="logo" />
+        </div>
+        <div className="navbar">
+            <ui className="nav-items">
+                <li className="list-items">Home</li>
+                <li className="list-items">About</li>
+                <li className="list-items">Contact us</li>
+                <li className="list-items">Cart</li>
+            </ui>
+        </div>
+    </div>
+  );
 };
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading/>);
+root.render(<Header />);
