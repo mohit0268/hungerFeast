5ad87a3d241e1819e6553008a1d5da68140a3f14
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-04-06 01:37:29 +0530

    Pages Routes added to src/pages folder

---
+++ src/pages/About.js
@@ -0,0 +1,6 @@
+const About = () =>{
+    return <h1>About page Coming Soon</h1>
+}
+
+export default About;
+
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,5 @@
+const ContactUs = () =>{
+    return <h1>Contact Us Page coming soon</h1>
+}
+
+export default ContactUs;
