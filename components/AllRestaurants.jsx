cff64af7f80a1a51f63c94187d64e26d0d3d7406
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-04-09 21:15:26 +0530

    bug Resolved

--- components/AllRestaurants.jsx
+++ components/AllRestaurants.jsx
@@ -6,45 +6,32 @@ const AllRestaurant = (props) => {
   const [listOfRestaurants, setlistofRestaurant] = useState([]);
   const { resData } = props;
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchApi();
-  },[]);
+  }, []);
 
-  const fetchApi = async() => {
+  const fetchApi = async () => {
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1458004&lng=79.0881546&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const json = await data.json();
-
     setlistofRestaurant(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
-  }
-  
-  return listOfRestaurants.length === 0 ? (<Shimmer />) :(
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="section">
       <div className="filter">
-        <div className="search-box">
-          <input 
-          type="text" 
-          className="filter-card-Search" 
-          />
-          <button className="filter-cards" 
-          onClick={()=>{
-            const filterRestaurants = listOfRestaurants.filter((res)=>{
-              return res.name;
-            })
-          }}>Search</button>
-        </div>
-        <div>
-        <button
-          className="btns btn-top-rated"
-          onClick={() => {
-            const filterList = listOfRestaurants.filter((res) => {
-              return res.avgRating > 4.3;
-            });
-            setlistofRestaurant(filterList);
-          }}
-        >
-          Ratings
-        </button>
-        </div>
+          <button
+            className="btns btn-top-rated"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter((res)=>{
+                return res.info.avgRating > 4;
+              });
+              setlistofRestaurant(filteredList);
+            }}
+          >
+            Ratings
+          </button>
       </div>
       <div className="res-container">
         {listOfRestaurants.map((restaurant) => (
