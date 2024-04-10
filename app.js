6de26002a33d32c37b4b4dfd592c2e7a5b0e9ee0
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-04-11 01:36:27 +0530

    Added react-router-dom,configured and add routing path

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from './components/Header'
 import AllRestaurant from "./components/AllRestaurants";
+import About from "./src/pages/About";
+import ContactUs from './src/pages/ContactUs';
+import Cart from "./src/pages/Cart"
+import { createBrowserRouter ,RouterProvider} from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -12,6 +16,26 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<AppLayout/>,
+  }
+  ,{
+    path:'/about',
+    element:<About/>,
+  },
+  {
+    path:'/contact',
+    element:<ContactUs/>,
+  },
+  {
+    path:'/cart',
+    element:<Cart/>,
+  }
+  
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router = {appRouter}/>);
