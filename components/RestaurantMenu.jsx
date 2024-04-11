a25b8c11d604b6843e8b169d0b3b081e525b83bf
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-04-11 14:14:31 +0530

    added new component

---
+++ components/RestaurantMenu.jsx
@@ -0,0 +1,15 @@
+const RestaurantMenu = () => {
+    return(
+    <div className="Menu">
+        <h1>Name</h1>
+        <h2>Menu</h2>
+        <ui>
+            <li>Pizza</li>
+            <li>Burger</li>
+            <li>Dessert</li>
+        </ui>
+    </div>            
+    )
+}
+
+export default RestaurantMenu;
