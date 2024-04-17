1288161b2e37b07283e9a32ed433f9800cd2f565
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-04-17 11:42:39 +0530

    Modified Restaurant menu api link

--- utils/constants.jsx
+++ utils/constants.jsx
@@ -1,5 +1,5 @@
 export const RESTAURANT_CARD_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const RESTAURANT_MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=427";
