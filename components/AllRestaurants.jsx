2fec3deda157a406b3c9d18c1734f0d1cbde609c
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-04-17 11:45:26 +0530

    Resolved the bug

--- components/AllRestaurants.jsx
+++ components/AllRestaurants.jsx
@@ -1,69 +1,70 @@
 import Cards from "./Cards";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { RESTAURANT_CARD_API } from "../utils/constants";
+import { Link } from "react-router-dom";
 
-const AllRestaurant = (props) => {
+const AllRestaurant = () => {
+
   const [listOfRestaurants, setlistofRestaurant] = useState([]);
-  const [filteredRestaurants,setFilteredRestaurants] = useState([]);
+  const [filteredRestaurant,setFilteredRestaurant] = useState([]);
   const [searchText,setSearchText] = useState("");
-  const { resData } = props;
 
-  useEffect(() => {
+  useEffect(()=>{
     fetchApi();
-  }, []);
+  },[])   
+
 
   const fetchApi = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1458004&lng=79.0881546&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch(RESTAURANT_CARD_API);
     const json = await data.json();
-    
-    setFilteredRestaurants(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
-    setlistofRestaurant(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
-  };
+    console.log(json);
+    setlistofRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilteredRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  
+  }
+return listOfRestaurants?.length === 0 ? 
+  (<Shimmer/>)
+  :
+  (
+      <div className="section">
+          <div className="filter">
+              <div className="filter-search">
+                  <input type="text" 
+                      className="search-text" 
+                      value={searchText} 
+                      onChange={(e)=>
+                          setSearchText(e.target.value)
+                      }
+                  />
+                  <button 
+                      className="search-btn"
+                      onClick={()=>{
+                          console.log(searchText);
+                          const filterRestaurant = listOfRestaurants.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                          
+                          setFilteredRestaurant(filterRestaurant);
+                      }}    
+                  >Search</button>
+              </div>
 
-  return listOfRestaurants.length === 0 ? (
-    <Shimmer />
-  ) : (
-    <div className="section">
-      <div className="filter">
-        <div className="filter-search">
-         <input type="text" 
-         className="search-text" 
-         value={searchText} 
-         onChange={(e)=>
-         setSearchText(e.target.value)
-         }/>
-         <button 
-         className="search-btn"
-         onClick={()=>{
-          console.log(searchText);
-          const filterRestaurantCards = listOfRestaurants.filter((res)=>{
-            return res.info.name.toLowerCase().includes(searchText.toLowerCase());
-          });
-          console.log(filterRestaurantCards);
-          setFilteredRestaurants(filterRestaurantCards);
+              <button
+                  className="btns btn-top-rated"
+                  onClick={() => {
+                      const filteredList = listOfRestaurants.filter((res)=>res.info.avgRating > 4);
+                      setlistofRestaurant(filteredList);
+                      console.log(listOfRestaurants)
+                  }}
+              >Ratings</button>
+          </div>
 
-         }}
-         >Search</button>
-        </div>
-          <button
-            className="btns btn-top-rated"
-            onClick={() => {
-              const filteredList = listOfRestaurants.filter((res)=>{
-                return res.info.avgRating > 4;
-              });
-              setlistofRestaurant(filteredList);
-            }}
-          >
-            Ratings
-          </button>
+          <div className="res-container">
+              {filteredRestaurant?.map((restaurant) => (
+                  <Link  key={restaurant.info.id} to={'/restaurants/' + restaurant.info.id }><Cards resData={restaurant} /></Link>
+              ))}
+          </div>
       </div>
-      <div className="res-container">
-        {filteredRestaurants.map((restaurant) => (
-          <Cards key={restaurant.info.id} resData={restaurant} />
-        ))}
-      </div>
-    </div>
-  );
+  )
 };
 
-export default AllRestaurant;
+export default AllRestaurant;  
--- components/RestaurantMenu.jsx
+++ components/RestaurantMenu.jsx
@@ -1,15 +1,45 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { RESTAURANT_MENU_API } from "../utils/constants";
+
 const RestaurantMenu = () => {
-    return(
+  const [resInfo, setResInfo] = useState(null);
+  
+  const {resId} = useParams();
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(RESTAURANT_MENU_API + resId + "&catalog_qa=undefined");
+    const json = await data.json();
+    console.log(json.data)
+    setResInfo(json.data)
+  }; 
+ 
+  if (resInfo === null) return <Shimmer/>;
+  const {name,cuisines,costForTwoMessage} = resInfo?.cards[2]?.card?.card?.info;
+  
+
+  const {itemCards} =resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+
+  return(
     <div className="Menu">
-        <h1>Name</h1>
-        <h2>Menu</h2>
-        <ui>
-            <li>Pizza</li>
-            <li>Burger</li>
-            <li>Dessert</li>
-        </ui>
-    </div>            
-    )
-}
+      <h1>{name}</h1>
+      <p>{cuisines.join(', ')} {costForTwoMessage}</p>
+      <h2>Menu</h2>
+      <ui>
+        {itemCards.map(item => (
+          <li key={item.card.info.id}>
+            {item.card.info.name}
+            </li>
+        ))}
+      </ui>
+
+      
+    </div>
+  );
+};
 
 export default RestaurantMenu;
