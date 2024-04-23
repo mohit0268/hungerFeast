e9b082ccf3beb6d57058d6059fc144db357aafda
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-04-24 01:39:01 +0530

    added custom hook

---
+++ utils/useRestaurantMenu.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { RESTAURANT_MENU_API } from "./constants";
+
+const useRestaurantMenu = (resId) => {
+    const [resInfo,setResInfo] = useState(null);
+
+    useEffect(()=>{
+        fetchMenu();
+    },[])
+    
+  const fetchMenu = async () => {
+    const data = await fetch(RESTAURANT_MENU_API + resId + "&catalog_qa=undefined");
+    const json = await data.json();
+    console.log(data);
+    setResInfo(json.data)
+  }; 
+  return resInfo;
+ 
+}
+
+export default useRestaurantMenu;
