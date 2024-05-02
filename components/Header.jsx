1b6aef07952fc0fe75a4f50a7d4553d11b996531
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-05-02 10:16:08 +0530

    online status added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,20 @@
 import { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
-
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnName,setbtnName] = useState('Login');
-  console.log('header render');
+  const online_status = useOnlineStatus();
 
     return (
       <div className="header">
         <div className="logo">
           <img src="./images/logo" alt="logo" />
+          
         </div>
         <div className="navbar">
           <ui className="nav-items">
+            <span>{online_status ? "🟢" : "🔴"}</span>
             <li className="list-items"><Link to='/'>Home</Link></li>
             <li className="list-items"><Link to='/about'>About</Link></li>
             <li className="list-items"><Link to='/contact'>Contact us</Link></li>
