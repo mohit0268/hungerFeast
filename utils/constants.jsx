d24e5b81cbe7b5f0cf341f1580dc9c410f8ab319
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-05-02 21:06:51 +0530

    logo url issue added and fixed

--- utils/constants.jsx
+++ utils/constants.jsx
@@ -3,3 +3,6 @@ export const RESTAURANT_CARD_API =
 
 export const RESTAURANT_MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+
+
+export const LOGO_URL = "https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg";
