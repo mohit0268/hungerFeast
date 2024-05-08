6b0958e3fed0c76c14e820254f18ecd022b81dae
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-05-08 17:07:30 +0530

    All Categories added

--- components/RestaurantsCategory.jsx
+++ components/RestaurantsCategory.jsx
@@ -1,7 +1,17 @@
-const RestaurantsCategory = () => {
+import ItemsList from "./ItemsList";
+
+const RestaurantsCategory = ({data}) => {
+    console.log(data);
     return(
-        <h1>title</h1>
+        <div className="w-6/12 m-auto bg-gray-100 shadow-lg my-4 p-4 rounded">
+            <div className="flex justify-between">
+            <span className="font-bold text-2xl">{data.title} ({data.itemCards.length})</span>
+            <span>▼</span>
+            </div>
+            <ItemsList items={data.itemCards}/>
+            
+        </div>
     );
 }
 
-export default RestaurantsCategory
+export default RestaurantsCategory;
