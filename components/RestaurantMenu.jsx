48482176288958eb049f6218a3f98a47d62388d0
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-05-12 23:24:57 +0530

    code added

--- components/RestaurantMenu.jsx
+++ components/RestaurantMenu.jsx
@@ -2,28 +2,31 @@ import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantsCategory from '../components/RestaurantsCategory'
+import { useState } from "react";
 
 const RestaurantMenu = () => {  
   
   const {resId} = useParams();
   const resInfo = useRestaurantMenu(resId);
+  const [showIndex, setShowIndex] = useState(0);
   
 
   if (resInfo === null) return <Shimmer/>;
   const {name,cuisines,costForTwoMessage} = resInfo?.cards[2]?.card?.card?.info;
-  
-
-  const {itemCards} =resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
-  console.log(resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card)
-  const categories = resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => c.card?.["card"]?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+  const {itemCards} =resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => c.card?.["card"]?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
 
 
   return( 
     <div className="text-center m-4">
       <h1 className="font-bold text-xl">{name}</h1>
       <p className="text-gray-500">{cuisines.join(', ')} {costForTwoMessage}</p>
-      {categories.map((category) => 
-      <RestaurantsCategory data = {category?.card?.card} />
+      {categories.map((category, index) => 
+      <RestaurantsCategory 
+      key={category?.card?.card.title} 
+      data = {category?.card?.card} 
+      showItems={index === showIndex ? true : false}
+      setShowIndex={()=>setShowIndex(index)} />
       )}
 
       
--- components/RestaurantsCategory.jsx
+++ components/RestaurantsCategory.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import ItemsList from "./ItemsList";
 
-const RestaurantsCategory = ({data}) => {
-    console.log(data);
-    return(
+const RestaurantsCategory = ({data,showItems,setShowIndex}) => {
+
+    const clickHandler = () => {
+        
+        setShowIndex();
+        
+    }
+     return(
         <div className="w-6/12 m-auto bg-gray-100 shadow-lg my-4 p-4 rounded">
-            <div className="flex justify-between">
-            <span className="font-bold text-2xl">{data.title} ({data.itemCards.length})</span>
+            <div className="flex justify-between cursor-pointer" onClick={clickHandler}>
+            <span className="font-bold text-xl">{data.title} ({data.itemCards.length})</span>
             <span>▼</span>
             </div>
-            <ItemsList items={data.itemCards}/>
+            {showItems && <ItemsList items={data.itemCards}/>}
             
         </div>
     );
