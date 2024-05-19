9b3329c6393cc72c77b286d1666c27cb3a6dddf9
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-05-20 00:33:09 +0530

    code added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,41 +1,66 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import { LOGO_URL } from "../utils/constants";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
 const Header = () => {
-  const [btnName,setbtnName] = useState('Login');
+  const [btnName, setbtnName] = useState("Login");
   const online_status = useOnlineStatus();
 
   //subscribing to the store using a selector
-    const cartItems = useSelector((store) => store.cart.items)
-    return (
-      <div className="flex justify-between bg-yellow-400 shadow-lg">
-        <div className="">
-          <img className='w-[100px] h-[100px] p-2 my-1 mx-2 rounded-full' src={LOGO_URL} alt="logo" />
-        </div>
-        <div className="flex items-center">
-          <ui className="hidden md:flex p-4 m-4">
-            <li className="px-4 list-none">Status:{online_status ? "🟢" : "🔴"}</li>
-            <li className="px-4 list-none"><Link to='/'>Home</Link></li>
-            <li className="px-4 list-none"><Link to='/about'>About</Link></li>
-            <li className="px-4 list-none"><Link to='/contact'>Contact us</Link></li>
-            <li className="px-4 list-none"><Link to='/cart'>Cart ({cartItems.length})</Link></li>
-            <button className="px-4" onClick={() => {
+  const cartItems = useSelector((store) => store.cart.items);
+
+  //
+  const MenuClickHandler = () => {};
+  return (
+    <div className="flex justify-between bg-yellow-400 shadow-lg ">
+      <div>
+        <img
+          className="w-[100px] h-[100px] p-2 my-1 mx-2 rounded-full"
+          src={LOGO_URL}
+          alt="logo"
+        />
+      </div>
+      <div className="flex items-center px-1">
+        <ui className="hidden md:flex mx-4">
+          <li className="px-4 list-none">
+            Status:{online_status ? "🟢" : "🔴"}
+          </li>
+          <li className="px-4 list-none">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-4 list-none">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="px-4 list-none">
+            <Link to="/contact">Contact us</Link>
+          </li>
+          <li className="px-4 list-none">
+            <Link to="/cart">Cart ({cartItems.length})</Link>
+          </li>
+          <button
+            className="px-4"
+            onClick={() => {
               btnName == "Login" ? setbtnName("Logout") : setbtnName("Login");
-            }}>{btnName}</button>
-            
-          </ui>
-          <button className="mx-8 text-amber-950  md:hidden" onClick={()=>{}}>
-              <i className="fa-solid fa-bars text-3xl"></i>
+            }}
+          >
+            {btnName}
           </button>
+        </ui>
+        <button
+          className="mx-8 text-amber-950  md:hidden"
+          onClick={MenuClickHandler}
+        >
+          <i className="fa-solid fa-bars text-3xl"></i>
+        </button>
+        <div className="fixed bg-red-500 inset-0 hidden">
           
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
