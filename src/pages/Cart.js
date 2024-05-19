2191212632c043444a5c0c4a7ce03483d559688e
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-05-20 00:28:32 +0530

    items list added in cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,15 @@
+import { useSelector } from "react-redux";
+import ItemsList from "../../components/ItemsList";
 const Cart = () =>{
-    return <h1>Cart page Coming Soon</h1>
+    const cartItems = useSelector((store) => store.cart.items);
+    return(
+        <div className="text-center m-4 p-4">
+            <h1 className="font-bold text-2xl">Cart</h1>
+            <div className="w-6/12 m-auto shadow-xl p-4">
+                <ItemsList items={cartItems}/>
+            </div>
+        </div>
+    )
 }
 
 export default Cart;
