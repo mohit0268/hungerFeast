8eba5666aac6a5ab31bb6c2c62064389b9bf3116
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-05-24 00:08:35 +0530

    modified code and made responsive in mobile mode

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,53 +13,79 @@ const Header = () => {
   const cartItems = useSelector((store) => store.cart.items);
 
   //
-  const MenuClickHandler = () => {};
+  const MenuClickHandler = () => {
+    const navDailog = document.querySelector("#nav-dailog");
+    navDailog.classList.toggle("hidden");
+  };
+
   return (
-    <div className="flex justify-between bg-yellow-400 shadow-lg ">
-      <div>
-        <img
-          className="w-[100px] h-[100px] p-2 my-1 mx-2 rounded-full"
-          src={LOGO_URL}
-          alt="logo"
-        />
-      </div>
-      <div className="flex items-center px-1">
-        <ui className="hidden md:flex mx-4">
-          <li className="px-4 list-none">
-            Status:{online_status ? "🟢" : "🔴"}
-          </li>
-          <li className="px-4 list-none">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="px-4 list-none">
-            <Link to="/about">About</Link>
-          </li>
-          <li className="px-4 list-none">
-            <Link to="/contact">Contact us</Link>
-          </li>
-          <li className="px-4 list-none">
-            <Link to="/cart">Cart ({cartItems.length})</Link>
-          </li>
+    <nav className="bg-yellow-400 shadow-lg">
+      <div className="flex justify-between">
+        <div>
+          <img
+            className="w-[100px] h-[100px] p-2 my-1 mx-2 rounded-full"
+            src={LOGO_URL}
+            alt="logo"
+          />
+        </div>
+        <div className="flex items-center px-1 mx-2">
+          <ui className="hidden md:flex mx-4">
+            <li className="px-4 list-none">
+              Status:{online_status ? "🟢" : "🔴"}
+            </li>
+            <li className="px-4 list-none">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="px-4 list-none">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="px-4 list-none">
+              <Link to="/contact">Contact us</Link>
+            </li>
+            <li className="px-4 list-none">
+              <Link to="/cart">Cart ({cartItems.length})</Link>
+            </li>
+            <button
+              className="px-4 bg-white text-center rounded-sm border border-black "
+              onClick={() => {
+                btnName == "Login" ? setbtnName("Logout") : setbtnName("Login");
+              }}
+            >
+              {btnName}
+            </button>
+          </ui>
           <button
-            className="px-4"
-            onClick={() => {
-              btnName == "Login" ? setbtnName("Logout") : setbtnName("Login");
-            }}
+            className="mx-8 text-amber-950 md:hidden"
+            onClick={MenuClickHandler}
           >
-            {btnName}
+            <i id="bars" className="fa-solid fa-bars text-3xl "></i>
           </button>
-        </ui>
-        <button
-          className="mx-8 text-amber-950  md:hidden"
-          onClick={MenuClickHandler}
-        >
-          <i className="fa-solid fa-bars text-3xl"></i>
-        </button>
-        <div className="fixed bg-red-500 inset-0 hidden">
-          
         </div>
       </div>
-    </div>
+
+
+
+      {/* //Responsiveness below 768px */}
+
+      <div id="nav-dailog" className="flex justify-center max-w-full">
+        <div className="absolute w-full rounded-lg">
+          <div className="block p-4 bg-white rounded-lg">
+            <li className="p-4 list-none rounded-lg my-1 shadow-xl bg-white hover:bg-gray-100">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="p-4 list-none rounded-lg my-1 shadow-xl bg-white hover:bg-gray-100">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="p-4 list-none rounded-lg my-1 shadow-xl bg-white hover:bg-gray-100">
+              <Link to="/contact">Contact us</Link>
+            </li>
+            <li className="p-4 list-none rounded-lg my-1 shadow-xl bg-white hover:bg-gray-100">
+              <Link to="/cart">Cart</Link>
+            </li>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
