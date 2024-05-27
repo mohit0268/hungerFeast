42ad59f1870aadd63ab5a940bdfb86bf1a8d88a5
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-05-27 22:32:19 +0530

    added more test cases

--- __test__/Header.test.js
+++ __test__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 import Header from "../components/Header";
 import { Provider } from "react-redux";
@@ -17,3 +17,33 @@ it("should load the header component with the login button", () => {
   const loginBtn = screen.getByRole('button',{name : 'Login'});
   expect(loginBtn).toBeInTheDocument();
 });
+
+it("should change the login button to logout on click", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <Header />
+        </Provider>
+      </BrowserRouter>
+    );
+    const loginBtn = screen.getByRole('button',{name : 'Login'});
+    fireEvent.click(loginBtn)
+    expect(loginBtn).toBeInTheDocument();
+
+    const logoutBtn = screen.getByRole('button',{name : 'Logout'});
+    
+    expect(logoutBtn).toBeInTheDocument();
+  });
+
+
+it("should render the header component with the cart Items", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <Header />
+        </Provider>
+      </BrowserRouter>
+    );
+    const cartItems = screen.getByText(/Cart /);
+    expect(cartItems).toBeInTheDocument();
+  });
