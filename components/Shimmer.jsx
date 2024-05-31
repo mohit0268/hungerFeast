fa4786c8756c58b6ae19a0cbe3cf7eff673cbfaf
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-05-31 11:31:14 +0530

    Loading effect added

--- components/Shimmer.jsx
+++ components/Shimmer.jsx
@@ -1,24 +1,11 @@
+
+
 const Shimmer = () => {
-    return(
-        <div className="shimmer-container">
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="w-16 h-16 border-4 border-blue-400 border-dashed rounded-full animate-spin"></div>
         </div>
-    );
-}
+      );
+};
 
 export default Shimmer;
