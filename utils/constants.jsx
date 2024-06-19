d8e4fcb66f8fbb9e10f22cd1b3a0adbd4bf9eef9
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-06-19 11:38:46 +0530

    updated the apiURL added corx proxy link

--- utils/constants.jsx
+++ utils/constants.jsx
@@ -1,8 +1,8 @@
 export const RESTAURANT_CARD_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const RESTAURANT_MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
 
 
 export const LOGO_URL = "https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg";
